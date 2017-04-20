ec400dc2a2d1da8b39c8f0ef715b4a5f13f28c8b
AlexxSap <alexxandrsap@mail.ru>
2017-04-20 09:07:42 +0300

    дописаны задачи для параграфа "Локальные переменные для объекта" https://learn.javascript.ru/closures-usage#tasks

--- closuresUsage.js
+++ closuresUsage.js
@@ -89,3 +89,36 @@ console.log(users);
 
 users.sort(byField('surName'));
 console.log(users);
+
+/*
+Создайте функцию filter(arr, func), которая получает массив arr и возвращает новый, в который входят только те элементы arr, для которых func возвращает true.
+Создайте набор «готовых фильтров»: inBetween(a,b) – «между a,b», inArray([...]) – "в массиве [...]". Использование должно быть таким:
+filter(arr, inBetween(3,6)) – выберет только числа от 3 до 6,
+filter(arr, inArray([1,2,3])) – выберет только элементы, совпадающие с одним из значений массива.
+*/
+
+var arr = [1, 2, 3, 4, 5, 6, 7];
+
+function filter(arr, func)
+{
+  return arr.filter(func);
+}
+
+function inBetween(a, b)
+{
+  return function(item)
+  {
+    return item >= a && item <= b;
+  }
+}
+
+function inArray(eArray)
+{
+  return function(item)
+  {
+    return eArray.indexOf(item) !== -1;
+  }
+}
+
+console.log(filter(arr, inBetween(3, 6))); // 3,4,5,6
+console.log(filter(arr, inArray([1, 2, 10]))); // 1,2
